2f3efe633038af947d3b0095061c88eca380b33d
sam <saramariasabogal2@gmail.com>
2025-04-27 13:43:39 -0500

    Eliminar archivos innecesarios

--- codigo/UQ/src/main/java/EstadoCita.java
+++
@@ -1,6 +0,0 @@
-public enum EstadoCita {
-    PENDIENTE,
-    CONFIRMADA,
-    CANCELADA,
-    FINALIZADA
-}
--- codigo/UQ/src/main/java/HistorialMedico.java
+++
@@ -1,6 +0,0 @@
-public interface HistorialMedico {
-
-    void registrarDiagnostico(String diagnostico);
-
-    void actualizarHistorial(String actualizacion);
-}
