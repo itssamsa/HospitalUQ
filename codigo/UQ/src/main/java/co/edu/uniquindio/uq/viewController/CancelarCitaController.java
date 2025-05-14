e082e005c09a4c5b6ab882fe89f544fc45c9c4a4
Jonatan D. Londoño C. <jonatand.londonoc1@uqvirtual.edu.co>
2025-05-14 15:27:38 -0500

    Se crea el boton volver en la cancelación de una cita

--- codigo/UQ/src/main/java/co/edu/uniquindio/uq/viewController/CancelarCitaController.java
+++ codigo/UQ/src/main/java/co/edu/uniquindio/uq/viewController/CancelarCitaController.java
@@ -6,11 +6,16 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class CancelarCitaController {
 
@@ -92,5 +97,17 @@ public class CancelarCitaController {
         alerta.setHeaderText(null);
         alerta.setContentText(mensaje);
         alerta.showAndWait();
+    }
+
+    @FXML
+    private void onVolver(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/co/edu/uniquindio/uq/IngresarPaciente.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();;
+        }
     }
 }
